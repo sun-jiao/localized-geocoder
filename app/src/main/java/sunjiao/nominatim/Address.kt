6b25a5eef82b43d39e9bd2cda5b0b26f0c48087a
Sun Jiao <xzzzf.dsx@gmail.com>
2020-03-24 22:16:50 +0800

    change address.kt

--- app/src/main/java/sunjiao/nominatim/Address.kt
+++ app/src/main/java/sunjiao/nominatim/Address.kt
@@ -2,6 +2,7 @@ package sunjiao.nominatim
 
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
+import org.json.JSONObject
 
 /**
  *
@@ -11,28 +12,7 @@ import androidx.annotation.Nullable
 
 //you can add more parameters if useful in your country.
 class Address(
-    @Nullable  val  building: String,
-    @Nullable  val  house_number: String,
-    @Nullable  val  road: String,
-    @Nullable  val  address29 : String,
-    @Nullable  val  allotments : String,
-    @Nullable  val  isolated_dwelling : String,     //1-2 households
-    @Nullable  val  hamlet: String,                 //small village with 100-200 people
-    @Nullable  val  locality : String,
-    @Nullable  val  farm : String,
-    @Nullable  val  school: String,
-    @Nullable  val  neighbourhood : String,
-    @Nullable  val  suburb : String,
-    @Nullable  val  city_district: String,
-    @Nullable  val  town: String,
-    @Nullable  val  city : String,
-    @Nullable  val  county: String,
-    @Nullable  val  region: String,
-    @Nullable  val  state_district: String,
-    @Nullable  val  state: String,
-    @NonNull   val  country: String,
-    @NonNull   val  country_code: String,
-
+    @NonNull   val  ranks : JSONObject,
     @NonNull   val  latitude :  Float,
     @NonNull   val  longitude : Float,
     @NonNull   val  display_name : String
--- app/src/main/java/sunjiao/nominatim/Nominatim.kt
+++ app/src/main/java/sunjiao/nominatim/Nominatim.kt
@@ -38,14 +38,11 @@ class Nominatim//according to Nominatim ToS, user agent is necessary
             .url(builder.toString())
             .removeHeader("User-Agent").addHeader("User-Agent", useragent)
             .build()
-        var addressObject : JSONObject = JSONObject()
         val response : Response = client.newCall(request).execute()
         if(response.body != null) {
             val jsonObject : JSONObject = JSONObject(response.body!!.string())
             if (jsonObject.isNull("error")){
-                addressObject = JSONObject(jsonObject.getString("address"))
-                addressObject.put("display_name",jsonObject.getString("display_name"))
-                return addressObject
+                return jsonObject
             } else
                 return null
         } else
@@ -56,30 +53,8 @@ class Nominatim//according to Nominatim ToS, user agent is necessary
     fun getAddress() : Address? {
         val json : JSONObject? = getJSON()
         if (json != null){
-            return Address(
-            json.getString("building"),
-            json.getString("house_number"),
-            json.getString("road"),
-            json.getString("address29"),
-            json.getString("allotments"),
-            json.getString("isolated_dwelling"),
-            json.getString("hamlet"),
-            json.getString("locality"),
-            json.getString("farm"),
-            json.getString("school"),
-            json.getString("neighbourhood"),
-            json.getString("suburb"),
-            json.getString("city_district"),
-            json.getString("town"),
-            json.getString("city"),
-            json.getString("county"),
-            json.getString("region"),
-            json.getString("state_district"),
-            json.getString("state"),
-            json.getString("country"),
-            json.getString("country_code"),
-                latitude, longitude,
-            json.getString("display_name"))
+            return Address( json.getJSONObject("address"), latitude, longitude, json.getString("display_name"))
+
         } else
             return null
     }
